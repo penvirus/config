719c4c93457e3fa77188645e91eaec850554e04b
Tzung-Bi Shih <penvirus@gmail.com>
2018-04-10 18:15:59 +0800

    active bashrc

--- start.sh
+++ start.sh
@@ -8,6 +8,9 @@ distribute()
 	mkdir -p ${HOME}/bin
 	cp bashrc ${HOME}/bin/
 	cp tmux.conf ${HOME}/bin/
+
+	sed -i /gg_config_gg/d ${HOME}/.bashrc
+	echo '. ${HOME}/bin/bashrc #gg_config_gg' >> ${HOME}/.bashrc
 }
 
 collect()
