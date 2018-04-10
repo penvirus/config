5374a4485aa811b7caa373921f55641481c10904
Tzung-Bi Shih <penvirus@gmail.com>
2018-04-10 18:01:44 +0800

    init

---
+++ start.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+distribute()
+{
+	cp -r vim ${HOME}/.vim
+	cp vimrc ${HOME}/.vimrc
+
+	mkdir -p ${HOME}/bin
+	cp bashrc ${HOME}/bin/
+	cp tmux.conf ${HOME}/bin/
+}
+
+collect()
+{
+	sleep 1
+}
+
+if [ -z "$1" ]; then
+	echo "$0 [0 | 1]"
+	echo "  0: distribute"
+	echo "  1: collect"
+else
+	if [ "$1" -eq 0 ]; then
+		distribute
+	else
+		collect
+	fi
+fi
