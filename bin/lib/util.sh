ffd687916dc61e9b481c84c043ec2af59c59167c
Tzung-Bi Shih <penvirus@gmail.com>
2021-06-01 11:00:51 +0800

    add common util

---
+++ bin/lib/util.sh
@@ -0,0 +1,35 @@
+_wait_host() {
+	local host=$1
+	local exp=$2
+	local e="ping -q -c 1 -W 1 ${host} >/dev/null"
+
+	if [ "${exp}" = "up" ]; then
+		e="! ${e}"
+	fi
+
+	echo -n "waiting ${host} ${exp}..."
+	while eval ${e}; do
+		echo -n '.'
+	done
+	echo "done"
+}
+
+wait_host_up() {
+	local host=$1
+
+	if [ -z "${host}" ]; then
+		return 1
+	fi
+
+	_wait_host "${host}" "up"
+}
+
+wait_host_down() {
+	local host=$1
+
+	if [ -z "${host}" ]; then
+		return 1
+	fi
+
+	_wait_host "${host}" "down"
+}
